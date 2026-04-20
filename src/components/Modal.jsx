35ed126534196d569fb9bc93726462ff0c963ab3
ps-jarh <pitambar132006@gmail.com>
2026-04-20 19:05:53 +0530

    changing the whole project. AI to be powered by GROQ

--- src/components/Modal.jsx
+++
@@ -1,9 +0,0 @@
-export default function Modal({ onClose, children, width = 490 }) {
-  return (
-    <div className="modal-overlay" onClick={e => { if (e.target === e.currentTarget) onClose() }}>
-      <div className="modal-box up" style={{ width }}>
-        {children}
-      </div>
-    </div>
-  )
-}
